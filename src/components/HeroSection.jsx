c50f471025d4663326fcec41d19827dab156c530
Fredrik Villo <fredrik.villo@gmail.com>
2024-10-13 19:39:32 +0200

    added photo to herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,10 @@
 export const HeroSection = () => {
     return (
-        <section className="h-[calc(100vh-122px)] bg-slate-500 flex flex-col justify-center items-center">
-            <h1>Verdens beste jente</h1>
-            <img className="w-1/3" src="/Luna1.jpg" alt="Project 4" />
+        <section className="h-[calc(100vh-122px)] bg-[#dac4b2] flex flex-col justify-center items-center">
+            <h1>Fredrik Villo</h1>
+            <img 
+            className="w-1/3 transform scale-[0.7] translate-x-[45rem] top-[7rem] rounded-t-full absolute" 
+            src="/meg_kreta.jpg" alt="Project 4" />
         </section>
         
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,23 +2,28 @@ function Nav() {
     return (
         <nav>
         <ul>
-            <li class="dropdown">
+            <li className="dropdown">
                 <a href="#">Courses</a>
-                <div class="dropdown-content">
+                <div className="dropdown-content">
                     <a href="#">Programming</a>
-                    <a href="#">Data Structures</a>
-                    <a href="#">Algorithms</a>
+                    <a href="#">Network and security</a>
+                    <a href="#">Algorithms and datastructures</a>
                 </div>
             </li>
-            <li class="dropdown">
+            <li className="dropdown">
                 <a href="#">Projects</a>
-                <div class="dropdown-content">
+                <div className="dropdown-content">
                     <a href="#">Web Development</a>
-                    <a href="#">Networking</a>
-                    <a href="#">Maths</a>
+                    <a href="#"></a>
+                    <a href="#"></a>
                 </div>
             </li>
-            <li><a href="#">About</a></li>
+            <li>
+                <a href="https://www.poist.no/about">About</a>
+            <div>
+                 
+                </div>
+            </li>
             <li><a href="#">Contact</a></li>
         </ul>
     </nav>
@@ -27,9 +32,9 @@ function Nav() {
 
 export function NavItem() {
     return (
-        <li class="dropdown">
+        <li className="dropdown">
         <a href="#">Courses</a>
-        <div class="dropdown-content">
+        <div className="dropdown-content">
             <a href="#">Programming</a>
             <a href="#">Data Structures</a>
             <a href="#">Algorithms</a>
