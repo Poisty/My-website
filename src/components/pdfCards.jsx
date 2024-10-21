4ec8b6ed8ecd4389a9faf6d6fd506511260742d3
Fredrik Villo <fredrik.villo@gmail.com>
2024-10-21 20:55:26 +0200

    fixed pdf cards

--- src/components/pdfCards.jsx
+++ src/components/pdfCards.jsx
@@ -5,7 +5,7 @@ export const PdfCard = ({ href, children, className }) => {
     return (
         <Card>
             <div className={`${className}`}>
-                <a href={href} target="_blank">
+                <a href={href} target="_blank" className="flex flex-col items-center">
                     <FaRegFilePdf className={`w-12 h-12`} />
                     <p>{children}</p>
                 </a>
--- src/pages/NetworkAndSecurity.jsx
+++ src/pages/NetworkAndSecurity.jsx
@@ -8,25 +8,18 @@ function NetworkAndSecurity() {
     return (
         <Content>
             <div className='flex flex-wrap gap-8 p-8'>
-                <PdfCard href="/LAB01.pdf">Rapport 1</PdfCard>
-                <PdfCard href="/Lab2_Rapport.pdfg">Rapport 1</PdfCard>
-                <PdfCard href="/Lab3_Rapport.pdf">Rapport 1</PdfCard>
-                <PdfCard href="/Lab_4_Dynamisk_ruting.pdf">Rapport 1</PdfCard>
+                <PdfCard href="/LAB01.pdf">HTTP/SMTP </PdfCard>
+                <PdfCard href="/Lab2_Rapport.pdf">DNS</PdfCard>
+                <PdfCard href="/Lab3_Rapport.pdf">TCP/UDP/ICMP</PdfCard>
+                <PdfCard href="/Lab_4_Dynamisk_ruting.pdf">Dynamisk ruting</PdfCard>
             </div>
             <div className="relative min-h-screen p-4 flex justify-center items-center">
                 <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
-                    <Card className="w-full md:w-1/2 p-8">
-                        <h1 className="text-2xl mb-4 font-bold font-raleway italic text-center">
-                            Fredrik Villo
-                        </h1>
-                        <h2 className="text-xl mb-4 font-raleway italic text-left leading-relaxed">
-                            NETTVERK!
-                        </h2>
-                    </Card>
+             
                     <img
-                        src="/meg.jpg"
+                        src="/Network.png"
                         alt="Fredrik Villo"
-                        className="w-60 h-60 rounded-[150px] object-cover"
+                        className="w-60 h-60 object-cover"
                     />
                 </div>
             </div>
@@ -34,20 +27,8 @@ function NetworkAndSecurity() {
             {/* Images below the card */}
             <div className="flex flex-col items-center gap-4 justify-center p-4">
                 <h3 className="text-center text-4xl">
-                    Mine verv
+                    
                 </h3>
-                <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
-                    <img
-                        src="/VOLTCircle2.png"
-                        alt="Image 1"
-                        className="w-32 h-32"
-                    />
-                    <img
-                        src="/nito.png"
-                        alt="Image 2"
-                        className="w-auto h-24"
-                    />
-                </div>
             </div>
         </Content>
 
