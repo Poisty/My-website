1a864c6d9795ed76d4103ae082152110124ee4d7
Poist69 <fredrik.villo@gmail.com>
2024-10-22 08:54:30 +0200

    added hover transform effect on logo and nav bar items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,11 +11,14 @@ function Nav() {
     }, [currentUrl]);
 
     return (
-        <nav className="bg-[#323035] px-4 sm:px-6 lg:px-8 xl:px-12 py-2 relative z-20"> {/* Add relative z-20 here */}
+        <nav className="bg-[#323035] px-4 sm:px-6 lg:px-8 xl:px-12 py-2 relative z-20">
             <div className="flex items-center relative justify-end h-12">
-                <Link to="/" className="z-10 left-0 -translate-y-1/3 absolute">
+                <Link
+                    to="/"
+                    className="z-10 left-0 -translate-y-1/3 absolute"
+                >
                     <img
-                        className="h-24 w-auto"
+                        className="h-24 w-auto transition-transform duration-300 hover:scale-110" // Size increase on hover
                         src="/poistlogo.png"
                         alt="poist labs logo"
                     />
@@ -26,23 +29,30 @@ function Nav() {
                             <NavItem to="/" text="Home" />
                         </li>
                     )}
-                    <li className="dropdown relative z-30"> {/* Added z-30 and relative */}
+                    <li className="dropdown relative z-30">
                         <Link className="" to="#">Courses</Link>
-                        <div className="dropdown-content absolute z-40"> {/* Added absolute and z-40 */}
+                        <div className="dropdown-content absolute z-40">
                             <Link to="/programming">Programmering</Link>
                             <Link to="/network&security">Network and security</Link>
                             <Link to="#">Algorithms and datastructures</Link>
                         </div>
                     </li>
+                    <li className="transition-transform duration-300 hover:scale-110">
                     <NavItem to="#" text="Portfolio"/>
-                    <NavItem to="about" text="About" />
-                    <NavItem to="#" text="Contact" />
+                    </li>
+                    <li className="transition-transform duration-300 hover:scale-110">
+                    <NavItem to="about" text="About"/>
+                    </li>
+                    <li className="transition-transform duration-300 hover:scale-110">
+                    <NavItem to="#" text="Contact"/>
+                    </li>
+                   
+                    
                 </ul>
             </div>
         </nav>
     );
 }
-
 
 export function NavItem({ to, text }) {
     return (
