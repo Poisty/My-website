41c752d8890e4227e65c87774f1829281c056951
Fredrik Villo <fredrik.villo@gmail.com>
2024-10-23 22:09:47 +0200

    .

--- src/pages/Algoritmer.jsx
+++ src/pages/Algoritmer.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Content } from '../components/layout/Content';
-import Card from '../components/layout/card';
-import { FaRegFilePdf } from "react-icons/fa6";
-import { PdfCard } from '../components/pdfCards';
 import Glass from '../components/layout/Glass';
-import Textbox from '../components/layout/TextBox';
-import Card_2 from '../components/layout/Card_2';
-import Circle from '../components/layout/Circle';
-import Searchbar from '../components/layout/Searchbar';
+
 
 function Algoritmer() {
     return (
