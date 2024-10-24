d95d2e02d3c767ed653f39dff317bd2278ec6e65
Fredrik Villo <fredrik.villo@gmail.com>
2024-10-24 20:14:07 +0200

    .

--- src/pages/Algoritmer.jsx
+++ src/pages/Algoritmer.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Content } from '../components/layout/Content';
 import Glass from '../components/layout/Glass';
-import Content_background from '../components/layout/Content_background';
-import './App.css';
+
 
 
 
